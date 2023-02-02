f7f000416a2f995d84e0498a1d3e835fa5a3f602
Zhe Song <zhs064@gmail.com>
2023-02-01 21:24:26 -0700

    local

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa1b5fkyyjgwsN1n5d7',
+        issuer: 'https://dev-41313304.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
